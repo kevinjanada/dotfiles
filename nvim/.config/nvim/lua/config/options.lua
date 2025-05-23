a840372550e8115d1836661539f18bf1d89cb413
Kevin Janada <kevin.janada@gmail.com>
2025-05-23 07:27:13 +0700

    hypr mouse sensitivity, nvim python

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,3 +4,18 @@
 
 -- vim.g.copilot_no_tab_map = true
 vim.opt.conceallevel = 2
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+-- Set tab settings specifically for Python files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
+})
