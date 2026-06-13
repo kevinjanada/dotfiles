8be255e3487be00021470638fe48deed6e1cb712
Kevin Janada <kevin.janada@gmail.com>
2026-06-13 13:22:34 +0700

    fixes after hyprland updates

--- hypr/.config/hypr/wallpaper.sh
+++ hypr/.config/hypr/wallpaper.sh
@@ -7,7 +7,7 @@ menu() {
 main() {
   choice=$(menu | wofi -c ~/.config/wofi/wallpaper -s ~/.config/wofi/style-wallpaper.css --show dmenu --prompt "Select Wallpaper:" -n)
   selected_wallpaper=$(echo "$choice" | sed 's/^img://')
-  swww img "$selected_wallpaper" --transition-type any --transition-fps 60 --transition-duration .5
+  awww img "$selected_wallpaper"
   # wal -i "$selected_wallpaper" -n --cols16
   wal -i "$selected_wallpaper"
   swaync-client --reload-css
